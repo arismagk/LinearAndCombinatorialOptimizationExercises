6fe22d9b2c9ab0a7aeff55d2da2d75613cad4a5d
Aris Magklaras <arismagklaras8115@gmail.com>
2022-04-19 19:41:36 +0200

    First exercises

--- Exercise1.py
+++ Exercise1.py
@@ -1,25 +1,3 @@
-from scipy.optimize import linprog
-
-#obj holds the coefficients from the objective function.
-#lhs_ineq holds the left-side coefficients from the inequality (red, blue, and yellow) constraints.
-#rhs_ineq holds the right-side coefficients from the inequality (red, blue, and yellow) constraints.
-
-obj = [3, 1] # Objective function coefficients 3x1 + x2
-
-lhs_ineq = [[-6,  -3],  # -6x1 - 3x2 <= -12  red left side
-            [-4,  -8],  # -4x1 - 8x2 <= -16  green left side
-            [6,    5],  #  6x1 + 5x2 <=  30  yellow left side
-            [6,    7]]  #  6x1 + 7x2 <=  36  orange left side
-
-rhs_ineq = [-12,   # -6x1 - 3x2 <= -12  red    right side
-            -16,   # -4x1 - 8x2 <= -16  green  right side
-            30,    #  6x1 + 5x2 <=  30  yellow right side
-            36]    #  6x1 + 7x2 <=  36  orange right side
-
-opt = linprog(c=obj, A_ub=lhs_ineq, b_ub=rhs_ineq, method="revised simplex")
-
-print(opt)
-
 from pulp import LpMaximize, LpProblem, LpStatus, lpSum, LpVariable
 
 # Create the model
@@ -46,10 +24,7 @@ print(f"objective: {model.objective.value()}")
 for var in model.variables():
     print(f"{var.name}: {var.value()}")
 
-import numpy as np
-import matplotlib.pyplot as plt
-from sympy.solvers import solve
-from sympy import Symbol
+
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -80,6 +55,42 @@ plt.plot(x1, x4, label=r'$6x1 +5x2\leq30$')
 plt.plot(x1, x5, label=r'$6x1 +7x2\leq36$')
 plt.plot(x1, x6, label=r'$3x1 + x2 = 15$')
 plt.plot(x1, x7, label=r'$3x1 + x2 = 13$')
+plt.xlim(0,8)
+plt.ylim(0,10)
+plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+plt.xlabel(r'$x1$')
+plt.ylabel(r'$x2$')
+
+plt.show()
+
+#(b)
+
+d = np.linspace(-2, 7, 300)
+x1, x2 = np.meshgrid(d, d)
+plt.imshow(((x1 >= 0) & (x2 >= 0) & (6 * x1 + 3* x2 >= 12) & (4 * x1 + 8 * x2 >= 16) & (6 * x1 + 5 * x2 <= 30) & (6 * x1 + 7 * x2 <= 36)).astype(int) ,
+                extent=(x1.min(),x1.max(),x2.min(),x2.max()),origin="lower", cmap="Greys", alpha = 0.3);
+
+x1 = np.linspace(0, 7, 2000)
+# x2 >= -6x1/3 +12/3
+x2 = -2 * x1 + 4
+# x2 >= 16/8 -4x1/8
+x3 = (16 - 4* x1) / 8
+# 6x1 + 5x2 <= 30
+x4 = (30 - 6 * x1) / 5
+# 6x1 + 7x2 <=36
+x5 = (36 - 6 * x1) / 7
+# objective function wih calculated maximum value: x1 + x2 = 8
+x6 = 8 - x1
+
+# objective function wih  less than the calculated maximum value: x1 + x2 = 3
+x7 = 3 - x1
+
+plt.plot(x1, x2, label=r'$6x1 +3x2\geq12$')
+plt.plot(x1, x3, label=r'$4x1 +8x2\geq16$')
+plt.plot(x1, x4, label=r'$6x1 +5x2\leq30$')
+plt.plot(x1, x5, label=r'$6x1 +7x2\leq36$')
+plt.plot(x1, x6, label=r'$x1 + x2 = 8$')
+plt.plot(x1, x7, label=r'$x1 + x2 = 3$')
 plt.xlim(0,8)
 plt.ylim(0,10)
 plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
--- Exercise5.py
+++ Exercise5.py
@@ -1,36 +1,72 @@
 import numpy as np
+from itertools import combinations
 from scipy import linalg
-import sympy
-import CalculateBasis as cb
+from math import comb
+import pandas as pd
 
+As = np.array([[1, -2, 3],
+              [5, 1, 2],
+              [1, 1, 1],
+              [2, -1, 1],
+              [3, -1, 1]])
+bs = np.array([5, 25, 125])
+cs = np.array([4, 2, 1])
 
 A = np.array([[1, 0, 0, 1, 0, 0],
               [4, 1, 0, 0, 1, 0],
               [8, 4, 1, 0, 0, 1]])
 
 b = np.array([5, 25, 125])
-c = np.array([4, 2, 1])
+c = np.array([4, 2, 1, 0, 0, 0])
 
-list_of_linear_independent_columns = cb.Calculator.CalculateAllBasisOfMatrix(A)
-#print(list_of_linear_independent_columns)
-A_transpose = A.transpose()
+m, n = A.shape
+print(n,m)
+print(A)
+print(b)
+print(c)
+print("\n")
+feas = 0
+nfeas = 0
+ninv = 0
+zl = [["x1", "x2", "x3", "z"]]
+zm = []
+for e in combinations(range(n), 3):
+    print(e)
+    B = A[:, e]
+    det = linalg.det(B)
+    if det != 0:
+        x = np.linalg.solve(B, b)
+        s = np.zeros(n)
+        for i in range(len(e)):
+            s[e[i]] = x[i]
+        print(s)
+        if np.any(x[1:] < 0):
+            print("Solution non feasible")
+            z = c.dot(s)
+            print(z)
+            zm.append(z)
+            nfeas += 1
+        else:
+            feas += 1
+            z = c.dot(s)
+            zl.append([s[0], s[1], s[2], z])
+            print(z)
+        print("\n")
+    else:
+        ninv += 1
+        print("Table cannot be inverted\n")
 
-bases = []
-for i in list_of_linear_independent_columns:
-    #print(A_transpose[i])
-    bases.append(A_transpose[i].transpose())
+print("There are", n, "variables and", m, "constrains (", n, "choose", m, "=", comb(n, m), "combinations )")
+print("There are", ninv, "arrays that cannot be inverted,", feas, "feasible solutions and", nfeas, "non feasible")
 
-for base in bases:
-    b_inv = linalg.inv(base)
-    print("Solution for base ", base, "is: ")
-    print(b_inv.dot(b))  # if >= 0 we have solution
+print(pd.DataFrame(zl[1:], columns=zl[0]))
 
+print(zm)
 
-
-
-
-
-
-
-
-
+for e2 in combinations(range(m), 3):
+    B = As[e2, :]
+    bl=[]
+    for i in e2:
+        bl.append(bs[i])
+    x = np.linalg.solve(B, bl)
+    print(e2,"\n",x,"\n",x.dot(cs))
